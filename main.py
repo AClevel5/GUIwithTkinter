0df85e7342a448cd87e151a70127d69dce6eacd2
alexcleveland <alexcleveland5@gmail.com>
2026-01-26 16:39:40 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import tkinter
+
+window = tkinter.Tk()
+window.title("My first GUI Program")
+window.minsize(width=500, height=500)
+
+#Labeles with Tkinter
+my_label = tkinter.Label(text="Label", font=("Arial", 25))
+my_label.pack()
+
+
+
+
+
+
+
+
+
+window.mainloop()
