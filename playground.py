032f86a7edfa45016c726b3be573cd9e2674f96d
alexcleveland <alexcleveland5@gmail.com>
2026-01-26 17:08:09 -0700

    class with kw args

--- playground.py
+++ playground.py
@@ -1,7 +1,25 @@
-def add(*args):
-    sum_of_args = 0
-    for n in args:
-        sum_of_args += n
-    print(sum_of_args)
+#positional arguments
+# def add(*args):
+#     sum_of_args = 0
+#     for n in args:
+#         sum_of_args += n
+#     print(sum_of_args)
+#
+# add(1,2,5,6,7,8,9)
 
-add(1,2,5,6,7,8,9)
+#Keyword arguments
+# def calculate(**Kwargs):
+#     for key, value in Kwargs.items():
+#         print(key, value)
+#
+# calculate(add=3, multiply=5)
+
+#Class with Key ward args. .get instead of brackets
+class Car:
+    def __init__(self, **kwargs):
+        self.make = kwargs.get("make")
+        self.model = kwargs.get("model")
+
+my_car = Car(make="Porsche", model="GT3RS")
+
+print(my_car.model)
