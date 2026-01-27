93500ef45dfe131ac8b60e8bee30a33e3ac56c42
alexcleveland <alexcleveland5@gmail.com>
2026-01-26 18:26:07 -0700

    buttons with tkinter

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
-import tkinter
+from tkinter import *
 
-window = tkinter.Tk()
+window = Tk()
 window.title("My first GUI Program")
 window.minsize(width=500, height=500)
 
 #Labeles with Tkinter
-my_label = tkinter.Label(text="Label", font=("Arial", 25))
+my_label = Label(text="Label", font=("Arial", 25))
 my_label.pack()
+
+def button_clicked():
+    print("clicked")
+    my_label["text"] = "You pressed the button"
+
+my_button = Button(text="Click Me", command=button_clicked)
+my_button.pack()
 
 
 
