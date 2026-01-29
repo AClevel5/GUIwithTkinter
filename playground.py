0ef80082c02dd7c5e5cc2b6a531f4994a341f9de
alexcleveland <alexcleveland5@gmail.com>
2026-01-28 17:57:09 -0700

    Moved different Tkinter options to playground and Grid Practice in Main

--- playground.py
+++ playground.py
@@ -1,25 +1,93 @@
-#positional arguments
-# def add(*args):
-#     sum_of_args = 0
-#     for n in args:
-#         sum_of_args += n
-#     print(sum_of_args)
-#
-# add(1,2,5,6,7,8,9)
+from tkinter import *
 
-#Keyword arguments
-# def calculate(**Kwargs):
-#     for key, value in Kwargs.items():
-#         print(key, value)
-#
-# calculate(add=3, multiply=5)
+window = Tk()
+window.title("My first GUI Program")
+window.minsize(width=500, height=500)
 
-#Class with Key ward args. .get instead of brackets
-class Car:
-    def __init__(self, **kwargs):
-        self.make = kwargs.get("make")
-        self.model = kwargs.get("model")
+#Labels with Tkinter
+my_label = Label(text="Label", font=("Arial", 25))
+my_label.pack()
 
-my_car = Car(make="Porsche", model="GT3RS")
+#Buttons with Tkinter
+def button_clicked():
+    print("clicked")
+    my_label["text"] = input.get()
 
-print(my_car.model)
+
+my_button = Button(text="Click Me", command=button_clicked)
+my_button.pack()
+
+#Entries
+entry = Entry(width=30)
+#Add some text to begin with
+entry.insert(END, string="Some text to begin with.")
+#Gets text in entry
+print(entry.get())
+entry.pack()
+
+#Text
+text = Text(height=5, width=30)
+#Puts cursor in textbox.
+text.focus()
+#Adds some text to begin with.
+text.insert(END, "Example of multi-line text entry.")
+#Get's current value in textbox at line 1, character 0
+print(text.get("1.0", END))
+text.pack()
+
+#Spinbox
+def spinbox_used():
+    #gets the current value in spinbox.
+    print(spinbox.get())
+spinbox = Spinbox(from_=0, to=10, width=5, command=spinbox_used)
+spinbox.pack()
+
+#Scale
+#Called with current scale value.
+def scale_used(value):
+    print(value)
+scale = Scale(from_=0, to=100, command=scale_used)
+scale.pack()
+
+#Checkbutton
+def checkbutton_used():
+    #Prints 1 if On button checked, otherwise 0.
+    print(checked_state.get())
+#variable to hold on to checked state, 0 is off, 1 is on.
+checked_state = IntVar()
+checkbutton = Checkbutton(text="Is On?", variable=checked_state, command=checkbutton_used)
+checked_state.get()
+checkbutton.pack()
+
+#Radiobutton
+def radio_used():
+    print(radio_state.get())
+#Variable to hold on to which radio button value is checked.
+radio_state = IntVar()
+radiobutton1 = Radiobutton(text="Option1", value=1, variable=radio_state, command=radio_used)
+radiobutton2 = Radiobutton(text="Option2", value=2, variable=radio_state, command=radio_used)
+radiobutton1.pack()
+radiobutton2.pack()
+
+
+#Listbox
+def listbox_used(event):
+    # Gets current selection from listbox
+    print(listbox.get(listbox.curselection()))
+
+listbox = Listbox(height=4)
+fruits = ["Apple", "Pear", "Orange", "Banana"]
+for item in fruits:
+    listbox.insert(fruits.index(item), item)
+listbox.bind("<<ListboxSelect>>", listbox_used)
+listbox.pack()
+window.mainloop()
+
+
+
+
+
+
+
+
+window.mainloop()
